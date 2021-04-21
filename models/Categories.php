27b476aebb5f1263af74dfe90b26afe3aad32d8c
Alireza mehri <36499843+alireza-mehri@users.noreply.github.com>
2021-04-21 11:22:25 +0430

    Categories
    create and edit and delete category

---
+++ models/Categories.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "categories".
+ *
+ * @property int $id
+ * @property string|null $name
+ */
+class Categories extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'categories';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+        ];
+    }
+}
